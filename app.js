b88e5639699c353fca2379e4d1784efd79537e6e
jsm-txt <hose153@gmail.com>
2021-03-29 18:56:23 -0500

    added api

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // Require Libraries
 const express = require('express');
 
+const Tenor = require("tenorjs").client({
+  // Replace with your own key
+  "Key": "LH1KPRWMQBMY", // https://tenor.com/developer/keyregistration
+  "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+  "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
+
 // App Setup
 const app = express();
 
@@ -13,7 +20,19 @@ app.set('view engine', 'handlebars');
 // Routes
 
 app.get('/', (req, res) => {
-  res.render('home')
+  // Handle the home page when we haven't queried yet
+  term = ""
+  if (req.query.term) {
+      term = req.query.term
+  }
+  // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+  Tenor.Search.Query(term, "10")
+      .then(response => {
+          // store the gifs we get back from the search
+          const gifs = response;
+          // pass the gifs as an object into the home page
+          res.render('home', { gifs })
+      }).catch(console.error);
 })
 
 app.get('/dog', (req, res) => {
